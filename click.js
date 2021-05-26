37685393348fa0e417d601a0cb27fa3404d4c3c0
yakeshiba <kebayashi0312@gmail.com>
2021-05-26 14:32:06 +0900

    test

---
+++ click.js
@@ -0,0 +1,10 @@
+var notes = [];
+
+$('.whitekey').on('click',function(){
+    $(this).addClass('whitekey_on');
+    
+});
+
+$('.blackkey').on('click',function(){
+    $(this).addClass('blackkey_on');
+});
